e45c764bdcab9af3d3cff5c87fb35b8c3dbbfc16
Michael Cruz <michael.cruz@uwalumni.com>
2017-04-09 16:44:26 -0700

    render html

--- server.py
+++ server.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def display():
     return "Hello, world."
 
+@app.route('/weblog')
+def weblog():
+    return render_template('index.html')
+
+app.run(debug=True, port=5000)
 if __name__ == '__main__':
     app.run()
