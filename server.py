eebc970c9da2e00a56e46711e3d2a98b17811051
Michael Cruz <michael.cruz@uwalumni.com>
2017-04-09 17:01:56 -0700

    query_parameter

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,6 +8,16 @@ def display():
 @app.route('/weblog')
 def weblog():
     return render_template('index.html')
+
+@app.route('/query')
+def query():
+    if 'file' in request.args:
+        query_filename = request.args.get('file')
+        return render_template(query_filename)
+    else:
+        return 'nothing specified'
+
+# http://localhost:5000/query?file=foo.html
 
 app.run(debug=True, port=5000)
 if __name__ == '__main__':
